39de117fc92dc39e62e06ccaeaa35038a018752c
shreyashack <shreyasmohite11123@gmail.com>
2020-10-24 19:41:52 +0530

    initial commit

---
+++ new.js
@@ -0,0 +1,6 @@
+function show() {
+    var x = document.getElementById("color").value;
+    document.getElementById("shows").innerHTML = x;
+    document.getElementById("box").style.backgroundColor = x;
+
+}
